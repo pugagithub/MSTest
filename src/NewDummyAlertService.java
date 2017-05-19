1a47f79b3bde73fda88f83a929ea25e97d06840c
pugagithub <pugal.sym@gmail.com>
2017-05-19 17:32:43 +0530

    Initial Version
    For MSTest

---
+++ src/NewDummyAlertService.java
@@ -0,0 +1,10 @@
+
+public class NewDummyAlertService implements AlertService {
+
+	@Override
+	public void alert(String message) {
+		// TODO Auto-generated method stub
+		System.out.println(message);
+	}
+
+}
